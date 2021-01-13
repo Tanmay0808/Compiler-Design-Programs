5d07ca680681748d345e1606a69923d8022cc5b2
Tanmay0808 <tanmay881999@gmail.com>
2021-01-13 13:57:33 +0530

    initial commit

---
+++ Count String Literals/input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//String literals are "inside double quotes"
+
+int main()
+{
+    //"This is a literal"
+    string s = "Tanmay";
+    cout<<"Name : "<<s<<"\n";
+    string t = "44";
+    cout<<"Roll No : "<<t<<"\n";
+    return 0;
+}
